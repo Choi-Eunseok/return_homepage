5e230a13cc2b6dd7c889152eb62793f7cb6dd742
DESKTOP-MO1VMR4\최은석 <ted12333@naver.com>
2021-08-08 20:40:04 +0900

    html test 1

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'text/html');
 // set Port
 app.set('port', process.env.PORT || 9000);
 
